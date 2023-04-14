411d254d1aefe2f6024aa0f8816c26b2a9602f32
Ray Luna <ray@hello-upswell.com>
2023-04-13 21:29:30 -0700

    refactor page structure and script files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-const year = new Date().getFullYear();
-const copyrightText = document.createTextNode(year);
+import Alpine from 'alpinejs';
+import {
+    getImageUrl,
+    fetchProfile,
+    fetchHobbies,
+    fetchFeaturedProjects
+} from './sanity';
 
-document.querySelector('footer #year').appendChild(copyrightText)
+window.Alpine = Alpine;
+
+const profile = await fetchProfile();
+const hobbies = await fetchHobbies();
+const projects = await fetchFeaturedProjects();
+
+function getThumbnailUrl(image) {
+    return getImageUrl(image).size(300, 300).url();
+}
+
+Alpine.store('profile', {
+    hobbies: hobbies.hobbies || [],
+    profile: profile,
+    image: profile.image ? getThumbnailUrl(profile.image) : '#',
+    projects: projects || [],
+    getThumbnailUrl,
+});
+
+Alpine.start();
