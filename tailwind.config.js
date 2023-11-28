45eb8ce36938fbe2c77205bcb06ae480c54d2bda
Ray Luna <leon.luna.ray@gmail.com>
2023-11-27 20:38:37 -0800

    setup fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,22 @@ export default {
       },
       center: true,
     },
-    extend: {},
+    extend: {
+      colors: {
+        alpine: {
+          10: '#6FFFE9',
+          20: '#5BC0BE',
+          30: '#3A506B',
+          40: '#1C2541',
+          50: '#0B132B',
+        },
+      },
+      fontFamily: {
+        dm: ['DM Sans', 'sans-serif'],
+        lora: ['Lora', 'serif'],
+        orbitron: ['Orbitron', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
