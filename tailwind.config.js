0be40ecb406b2204160d98a131890daa15d0ed2f
Ray Luna <leon.luna.ray@gmail.com>
2023-11-27 22:16:00 -0800

    hero setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,13 +27,11 @@ export default {
       animation: {
         none: 'none',
         bounce: 'bounce 1s infinite',
-        line: 'line 5s cubic-bezier(0.07, 0.15, 0.6, .75) infinite',
-        'line-reveal': 'line-reveal 0.5s cubic-bezier(0.07, 0.15, 0.6, 0.75)',
         ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
         pulse: 'pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
         slide: 'slide 1s ease-in-out infinite alternate, slide-out 1s ease-in-out 1s infinite alternate',
-        spin: 'spin 5s linear infinite',
-        'spin-slow': 'spin 3s linear infinite',
+        spin: 'spin 2s linear infinite',
+        'spin-slow': 'spin 5s linear infinite',
         wiggle: 'wiggle 4s ease-in-out infinite',
       },
       keyframes: {
@@ -45,18 +43,6 @@ export default {
           '50%': {
             transform: 'none',
             animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
-          },
-        },
-        line: {
-          '0%, 75%, 100%': { top: '31px' },
-          '15%, 60%': { top: 0 },
-        },
-        'line-reveal': {
-          '0%, 75%, 100%': {
-            clipPath: 'polygon(0 0, 100% 0, 100% 0, 0 0)',
-          },
-          '15%, 60%': {
-            clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
           },
         },
         ping: {
