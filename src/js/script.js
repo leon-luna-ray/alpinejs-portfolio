0c2a170c08aef8b425c8992e6138cce4f20be820
Ray Luna <leon.luna.ray@gmail.com>
2023-11-27 22:39:34 -0800

    setup darkmode functionaliyt

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 import Alpine from 'alpinejs';
+import Darkmode from 'darkmode-js';
+
 import {
     getImageUrl,
     fetchProfile,
@@ -11,10 +13,20 @@ window.Alpine = Alpine;
 const profile = await fetchProfile();
 const hobbies = await fetchHobbies();
 const projects = await fetchFeaturedProjects();
+const darkmode = new Darkmode();
 
 function getThumbnailUrl(image) {
     return getImageUrl(image).size(300, 300).url();
 }
+
+const isDarkModeActive = () => ({
+    'border-red': darkmode.isActivated(),
+})
+
+Alpine.store('ui', {
+    darkmode,
+    isDarkModeActive,
+});
 
 Alpine.store('profile', {
     hobbies: hobbies.hobbies || [],
