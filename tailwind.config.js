7187b8d72875ee11427620b35fbfdeda9d035927
Ray Luna <leon.luna.ray@gmail.com>
2023-11-27 19:43:49 -0800

    setup state development stage w/tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,html}'],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        lg: '2rem',
+      },
+      center: true,
+    },
     extend: {},
   },
   plugins: [],
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite';
-// import autoprefixer from 'autoprefixer';
-// import ssr from 'vite-plugin-ssr/plugin'
 
 export default defineConfig({
   build: {
     target: 'esnext',
   },
-  // css: {
-  //   postcss: {
-  //     plugins: [
-  //       autoprefixer(),
-  //     ],
-  //   },
-  // },
 });
