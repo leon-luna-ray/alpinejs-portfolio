40e0db37a0cd4519ee7440580f43a86895863948
Ray Luna <ray@hello-upswell.com>
2023-11-28 13:42:43 -0800

    add dark mode funcitonality

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,html}'],
+  darkMode: 'class',
   theme: {
     container: {
       padding: {
